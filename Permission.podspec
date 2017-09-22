8bc97f55736b1d33c08e7455c3f1fee08143a3ff
Yannick Loriot <yannick.loriot@gmail.com>
2017-09-22 17:25:24 +0200

    Update to Swift4, Xcode 9

--- Permission.podspec
+++ Permission.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "Permission"
-  s.version      = "2.0.5"
+  s.version      = "2.0.6"
   s.license      = { :type => "MIT" }
   s.homepage     = "https://github.com/workandplay/Permission"
   s.author       = { "Damien" => "damien@delba.io" }
--- Source/PermissionTypes/Camera.swift
+++ Source/PermissionTypes/Camera.swift
@@ -26,7 +26,7 @@ import AVFoundation
 
 internal extension Permission {
     var statusCamera: PermissionStatus {
-        let status = AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
+        let status = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
         
         switch status {
         case .authorized:          return .authorized
@@ -41,7 +41,7 @@ internal extension Permission {
             return
         }
         
-        AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo) { _ in
+        AVCaptureDevice.requestAccess(for: AVMediaType.video) { _ in
             callback(self.statusCamera)
         }
     }
